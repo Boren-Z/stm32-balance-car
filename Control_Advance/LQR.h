dd0871dcd9398bb86f129c052a21e4c8f6c12a8a
Boren Zheng <134754144+Boren-Z@users.noreply.github.com>
2026-09-11 17:19:59 +0200

    Creat new files as preparation for Kalman Filter And LQR

---
+++ Control_Advance/LQR.h
@@ -0,0 +1,9 @@
+#ifndef	__LQR_H
+#define	__LQR_H
+
+#include "stm32f10x.h"
+
+
+
+#endif	//__LQR_H
+
---
+++ Data/KalmanFilter.h
@@ -0,0 +1,8 @@
+#ifndef	__KalmanFilter_H
+#define	__KalmanFilter_H
+
+#include "stm32f10x.h"
+
+
+
+#endif	//__KalmanFilter_H
